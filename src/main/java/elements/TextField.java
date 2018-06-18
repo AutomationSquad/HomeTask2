830e82221bc0a74e0cf24520724ae9e164c4073d
Strohetska <oksistrog@gmail.com>
2018-06-18 22:32:24 +0300

    HomeWork_Interface

--- src/main/java/elements/TextField.java
+++ src/main/java/elements/TextField.java
@@ -1,13 +1,27 @@
 package elements;
 
-public class TextField extends TextComponent {
+public class TextField extends TextComponent implements IVisible {
 
     public TextField(int width, int height, String text) {
-
-        super(width, height, text);
     }
+
+    @Override
+    public void inputText() {
+        System.out.println("TextField");
+    }
+
     @Override
     public void setSize (int width, int height) {
         System.out.println("TextField size is: " + width + " - width " + height + " - height");
+    }
+
+    @Override
+    public void getText() {
+        System.out.println("Textfield text");
+    }
+
+    @Override
+    public void isDisplayed() {
+        System.out.println("Text is displayed");
     }
 }
